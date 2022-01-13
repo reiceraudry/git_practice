ad93bce6f65356e8c911f95d7cac455ee297a452
Reice <reiceraudry@gmail.com>
2022-01-13 09:12:08 -0600

    Figure out Git

---
+++ Random Message/randomMessage.js
@@ -0,0 +1,9 @@
+let verbs = ['goes', 'runs']
+let subjects = ['Reice', 'Jace']
+let preps = ['to the store', 'to the bar']
+
+const basicSentence = () => {
+    return `${subjects[1]} ${verbs[1]} ${preps[1]}`
+};
+
+console.log(basicSentence())
